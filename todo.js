fba0ca1045d9a25f29447726300df6bf04b6151a
rahul rajput <rahulrajput7358@gmail.com>
2021-05-19 00:50:33 +0530

    now adding local storage feature

--- todo.js
+++ todo.js
@@ -1,7 +1,7 @@
 // taking refrences and adding addEventListeners
 let addBtn = document.getElementById("addBtn");
 let listContainer = document.getElementById("listContainer");
-let allLists = document.getElementsByClassName("lists");
+
 let uniqueNumForLists = 0;
 addBtn.addEventListener("click", addList);
 document.addEventListener("keypress", (e) => {
@@ -14,15 +14,19 @@ document.addEventListener("keypress", (e) => {
 function addList() {
   let inputBox = document.getElementById("toDoInput");
   if (inputBox.value != "") {
+    // here i wrote logic for the text --> "you have not added anything yet"
     if (
       listContainer.children.length != 0 &&
       listContainer.children[0].id == "temp"
     ) {
       listContainer.removeChild(document.getElementById("temp"));
     }
+    // to add local storage i call this function
+    let listsObj = addLocalStorage(inputBox.value);
+    // here i am creating new list and added that input value to that
+
     let newList = document.createElement("div");
     newList.classList = "lists";
-    uniqueNumForLists++;
     newList.id = `${uniqueNumForLists}`;
     newList.onclick = listWorkDone.bind(newList, uniqueNumForLists);
     newList.innerHTML = `<i class="fas fa-check"></i>
@@ -30,7 +34,7 @@ function addList() {
     <i class="fas fa-times" onclick='deleteList(${uniqueNumForLists})'></i>`;
     listContainer.append(newList);
     inputBox.value = "";
-    allLists = document.getElementsByClassName("lists");
+    uniqueNumForLists++;
   }
 }
 // adding list logic ends here
@@ -41,13 +45,31 @@ function listWorkDone(argu) {
   let list = document.getElementById(argu);
   if (list.classList.contains("workDone")) {
     list.classList.remove("workDone");
+    list.children[0].classList.remove("checkDisplayVisible");
   } else {
     list.classList.add("workDone");
+    list.children[0].classList.add("checkDisplayVisible");
   }
 }
 //list work done logic ends here
 
-///deleleting tha lists
+///deleting the lists
 function deleteList(argu) {
   listContainer.removeChild(document.getElementById(argu));
+}
+
+///storing lists in local storage logic starts form here
+
+function addLocalStorage(inputVal) {
+  let localStorageLists = localStorage.getItem("lists");
+  if (localStorageLists == null) {
+    listsObj = [];
+  } else {
+    listsObj = JSON.parse(localStorageLists);
+  }
+  // console.log(listsObj);
+  // console.log(inputVal);
+  listsObj.push(inputVal);
+  localStorage.setItem("lists", JSON.stringify(listsObj));
+  return listsObj;
 }
